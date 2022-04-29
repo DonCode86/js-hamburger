9fc2251939260ada9b35cd40f722b423e6043c0b
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-04-29 16:13:46 +0200

    aggiunta chiusura menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const hamburgerMenu = document.getElementsByClassName("hamburger-menu") [0]; 
 const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
 openMenu.addEventListener("click",
     function () {
     hamburgerMenu.style.display= "block";
+    }
+);
+closeMenu.addEventListener("click",
+    function () {
+    hamburgerMenu.style.display= "none";
     }
 );
 
